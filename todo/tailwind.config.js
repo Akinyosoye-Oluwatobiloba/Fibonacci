1db96ab9d34c31cc31f0ed55fe47ec1c45380303
Tech-Kid <akinyosoyegodwin555@gmail.com>
2022-10-26 10:33:25 +0100

    fibonacci

---
+++ todo/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        gray:{
+          400:'#969798',
+          500:'#666666',
+          700:'#333333',
+          900:'#174533'
+        }
+    }
+  },
+  plugins: [],
+}
